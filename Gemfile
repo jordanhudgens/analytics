7c92db82a72f02ce0bfcca506e9ee14aa9e63eb2
Jordan Hudgens <jordan@wow.com>
2014-02-01 22:46:52 -0600

    debugging js

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ source 'http://rubygems.org'
 gem 'sinatra'
 gem 'shotgun'
 
+gem 'data_mapper'
+gem 'sqlite3'
+gem 'dm-sqlite-adapter'
+
 group :development, :test do
   gem 'rspec', '2.13.0'
 
--- analytics.rb
+++ analytics.rb
@@ -1,4 +1,25 @@
 require 'sinatra'
+require 'data_mapper'
+require 'dm-core'
+require 'dm-timestamps'
+
+DataMapper::setup(:default, 'sqlite3://#{Dir.pwd}/analytics.rb')
+
+class Link
+    include DataMapper::Resource
+
+    property :id, Serial
+    property :link, String
+    property :created_at, DateTime
+    property :updated_at, DateTime
+
+end
+
+
+# set utf-8 for outgoing
+before do
+    headers "Content-Type" => "text/html; charset=utf-8"
+end
 
 class Database
   def initialize
@@ -15,10 +36,14 @@ class Database
 end
 
 get '/' do
-  "Welcome to the homepage"
+    @title = "Homepage"
+    erb :welcome
 end
 
 post '/request' do
   Database.new.insert_request(params)
   ''
+end
+
+get '/click/:id' do
 end
